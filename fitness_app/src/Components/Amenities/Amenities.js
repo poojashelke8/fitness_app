b3ff7905e38b6bccb6069ee1eb9b9002a38ecc7b
poojashelke8 <poojabshelke0807@gmail.com>
2023-10-09 22:44:26 +0530

    added card component to the amenities component

--- fitness_app/src/Components/Amenities/Amenities.js
+++ fitness_app/src/Components/Amenities/Amenities.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Card from "./Card";
 import './Amenities.css';
 
 function Amenities(){
     return(
-        <div>
-            Amenities
+        <div className="amenities">
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
     )
 }
---
+++ fitness_app/src/Components/Amenities/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Card.css';
+
+function Card(){
+    return(
+        
+        <div className="card">
+            <div className="cardData">
+                Card
+            </div>
+        </div>
+    )
+}
+
+export default Card;
