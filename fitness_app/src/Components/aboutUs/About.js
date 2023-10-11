be20ad8f91e611ccf6d4aa00ed4b7d795628c2dc
poojashelke8 <poojabshelke0807@gmail.com>
2023-10-11 21:26:08 +0530

    added some style and divs

--- fitness_app/src/Components/aboutUs/About.js
+++ fitness_app/src/Components/aboutUs/About.js
@@ -6,14 +6,17 @@ function About() {
     return (
         <div className="aboutUs">
             <div className="aboutText">
-                <h1>500 +</h1>
-                <h2>Satisfied Customers</h2>
+                <h1>500+</h1><br/>
+                <h2>Satisfied Customers</h2><br/>
                 <p>"Our growing community of satisfied customers 
                     is a testament to the trust and confidence placed in us. 
                     With each passing day, our numbers continue to soar, 
                     fueled by the unwavering support and positive feedback we receive. 
                     We are deeply grateful for the trust you've placed in our services, 
                     and we remain committed to delivering excellence that surpasses your expectations.</p>
+            </div>
+            <div className="aboutText2">
+                <h2>Get in shape and training at the gym or home.</h2>
             </div>
 
             <img id="aboutimg" src={myImage} alt="My Image" />
