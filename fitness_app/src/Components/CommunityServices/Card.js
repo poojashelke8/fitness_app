f4792e8329ed466e8f571757b24780671d46598e
poojashelke8 <poojabshelke0807@gmail.com>
2023-10-15 21:10:18 +0530

    made some changes in css also added community pages

---
+++ fitness_app/src/Components/CommunityServices/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card(props) {
+    return (
+        <div>
+            <div>
+                <h2>Championship</h2>
+                <h2>Mindfullness</h2>
+                <h2>Yoga</h2>
+                <h2>Engage and Educate</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- fitness_app/src/Components/CommunityServices/Community.js
+++ fitness_app/src/Components/CommunityServices/Community.js
@@ -1,10 +1,17 @@
 import React from "react";
 import './Community.css';
+import Card from "./Card";
 
 function Community(){
     return (
         <div className="community">
-            Community
+             <div className="left">
+            <h1>Body and Soul Fitness</h1>
+            </div>
+            
+            <div className="right">
+                <Card/>
+            </div> 
         </div>
     )
 }
