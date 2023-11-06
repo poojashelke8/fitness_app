64f420bb06ca2646503d7f21785fe77a4ebdc7f7
poojashelke8 <poojabshelke0807@gmail.com>
2023-11-06 19:30:54 +0530

    added footer bar and also completed the enquiry form

--- fitness_app/src/Components/Contact/Contact_enquiry.js
+++ fitness_app/src/Components/Contact/Contact_enquiry.js
@@ -12,51 +12,60 @@ function Contact_enquiry() {
                     <form>
                         {/* <input placeholder="Name" />
                         <input placeholder="Email" /> */}
-                        <input type="text" name="name" class="form-control"
-                            id="name" placeholder="Full Name" data-rule="minlen:4"
-                            data-msg="Please enter at least 4 chars"></input>
+                        <div className="form_row1">
+                            <input type="text" name="name" class="form-control"
+                                id="name" placeholder="Full Name" data-rule="minlen:4"
+                                data-msg="Please enter at least 4 chars"></input>
 
-                        <input type="email" class="form-control" name="email"
-                            id="email" placeholder="Your Email" data-rule="email"
-                            data-msg="Please enter a valid email"></input>
+                            <input type="email" class="form-control" name="email"
+                                id="email" placeholder="Your Email" data-rule="email"
+                                data-msg="Please enter a valid email"></input>
 
-                        <input type="tel" name="phone" class="form-control"
-                            id="phone" placeholder="Mobile No" data-rule="minlen:10"
-                            data-msg="Please enter at least 10 chars"></input>
+                        </div>
 
-                        <input type="text" class="form-control" placeholder="City" name="city"
-                            id="city" data-rule="minlen:4" data-msg="Please enter at least 4 chars"></input>
+                        <div className="form_row2">
+                            <input type="tel" name="phone" class="form-control"
+                                id="phone" placeholder="Mobile No" data-rule="minlen:10"
+                                data-msg="Please enter at least 10 chars"></input>
 
-                        <textarea class="form-control" name="message" rows="3"
-                            data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                            <input type="text" class="form-control" placeholder="City" name="city"
+                                id="city" data-rule="minlen:4" data-msg="Please enter at least 4 chars"></input>
 
-                        <button>Send Message</button>
+                        </div>
+                        <div className="form_row3">
+                            <textarea class="form-control" name="message" rows="3" id="message"
+                                data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+
+                        </div>
+                        <button id="send_btn">Send Message</button>
                     </form>
 
                 </div>
-                {/* <form>
-                    <label>Enter your name:
-                        <input
-                            type="text"
-                            name="username"
-                        // value={inputs.username || ""} 
-                        // onChange={handleChange}
-                        />
-                    </label>
-                    <label>Enter your age:
-                        <input
-                            type="number"
-                            name="age"
-                        //   value={inputs.age || ""} 
-                        //   onChange={handleChange}
-                        />
-                    </label>
-                    <input type="submit" />
-                </form> */}
             </div>
             <div className="footer">
+                <div className="footer_1">
+                    <h2 id="heading_f">Suprimo Gym</h2>
+                    <p id="contact">Sweat, lift, and conquer your fitness goals with us.
+                         Your journey to a stronger, healthier you starts here. <br/><br/>
+                         Join our gym now!</p>
+                </div>
+                <div className="footer_1">
+                    <h2>Get Assistance</h2>
+                    <p id="contact">
+                        Phone: 1234567890<br/>
+                        Email: suprimo@Gym.co.in
+                    </p>
+                </div>
+                <div className="footer_1">
+                    <h2>Locate Us</h2>
+                    <p id="contact">FAQ's</p>
+                    <a href="#" >Centers</a>
+                </div>
+            </div>
+            {/* <br/> */}
 
-                Footer
+            <div className="copy_rights">
+                copyrights
             </div>
         </>
     )
