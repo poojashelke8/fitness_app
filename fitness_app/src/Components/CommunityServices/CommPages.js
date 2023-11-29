ceafee760243ede5ec12981c45d021fd5a6e66d2
poojashelke8 <poojabshelke0807@gmail.com>
2023-11-29 23:07:51 +0530

    added description for pages and some style

--- fitness_app/src/Components/CommunityServices/CommPages.js
+++ fitness_app/src/Components/CommunityServices/CommPages.js
@@ -8,22 +8,25 @@ const CommPages = () => {
     const data = {
         0: {
             name: "Championship",
-            desc: "Description for Championship category",
+            desc: "Championships bring out the best qualities in each member and trainer, who participate across various activities like Football, Powerlifting, Athletics, Cricket, Boxing, Calisthenics, Paintball etc, which are played at team and centre levels to encourage group involvement and centre level engagement activities.",
             images: "https://images.unsplash.com/photo-1520529301226-42abc4cd766b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         1: {
             name: "Mindfulness",
-            desc: "Description for Mindfulness category",
+            desc: `Mindfulness is a practice that encourages individuals to be fully present in the moment, fostering a heightened awareness of their thoughts, feelings, and surroundings. 
+            It encompasses a state of non-judgmental attention, promoting mental clarity, emotional balance, and overall well-being. At my gym, the commitment to holistic health extends beyond physical fitness, and mindfulness is recognized as a crucial component of a comprehensive well-being approach. The gym has implemented mindfulness programs that cater to the diverse needs and preferences of its members. 
+            From dedicated meditation spaces equipped with calming ambiance to regular mindfulness classes led by experienced instructors, the gym provides ample opportunities for individuals to integrate mindfulness practices into their fitness routines. 
+            Additionally, the gym has incorporated mindfulness elements into its physical training sessions, emphasizing the importance of being present and focused during exercises, thereby enhancing the mind-body connection. By prioritizing mindfulness, my gym not only aims to improve the mental resilience of its members but also fosters an environment where individuals can experience a deeper sense of self-awareness and tranquility alongside their physical fitness journey.`,
             images: "https://images.unsplash.com/photo-1506126613408-eca07ce68773?q=80&w=1399&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         2: {
             name: "Yoga",
-            desc: "Description for Yoga category",
+            desc: "Yoga takes center stage in the wellness landscape at my gym, where a thoughtfully designed program caters to practitioners of all levels. The gym boasts dedicated yoga studios with a serene atmosphere, offering a range of classes led by certified instructors. From foundational sessions for beginners to advanced classes incorporating mindfulness and intricate postures, the gym provides a diverse and inclusive yoga experience. Specialized workshops, including hot yoga and aerial yoga, add variety to the offerings. This holistic approach underscores the gym's commitment to fostering not just physical fitness but also mental clarity and emotional well-being. Through yoga, my gym creates a space where members can seamlessly integrate the benefits of mindfulness into their overall fitness journey, promoting balance, flexibility, and inner peace.",
             images: "https://images.unsplash.com/photo-1562088287-bde35a1ea917?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         3: {
             name: "Engage and educate",
-            desc: "Description for Engage and educate category",
+            desc: "My gym is committed to the dual pillars of engaging and educating its members, ensuring a well-rounded fitness experience. Beyond traditional workout routines, the gym hosts regular seminars, workshops, and interactive sessions led by fitness experts and nutritionists. These events not only engage members in diverse aspects of health and wellness but also provide valuable education on topics ranging from optimal nutrition to effective training techniques. The gym leverages modern technology, offering personalized workout apps and virtual coaching sessions to enhance member engagement and knowledge. By fostering a culture of continuous learning and interaction, my gym goes beyond the physical aspects of fitness, empowering its members with the knowledge and motivation needed to make informed and sustainable lifestyle choices, creating a community that is both active and well-informed.",
             images: "https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
     };
@@ -40,8 +43,8 @@ const CommPages = () => {
                 CommPages {item.name}
             </h2>
             <div className='pageContent'>
-                <h3>{item.desc}</h3>
-                {item.images && <img className= "pageImage"src={process.env.PUBLIC_URL + item.images} alt={item.name} />}
+                <p id='content1'>{item.desc}</p>
+                {item.images && <img id="content2" src={process.env.PUBLIC_URL + item.images} alt={item.name} />}
             </div>
         </div>
 
