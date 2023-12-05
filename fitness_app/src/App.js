5c0c7073f360d4d161c828b19a26149c8e49380c
poojashelke8 <poojabshelke0807@gmail.com>
2023-12-05 21:19:23 +0530

    made some final changes and will do according to further learning. Thanks..!

--- fitness_app/src/App.js
+++ fitness_app/src/App.js
@@ -11,7 +11,7 @@ import Classes from './Components/Home/Classes';
 import Team from './Components/Home/Team';
 import CommPages from './Components/CommunityServices/CommPages';
 import Footer from './Components/Contact/Footer';
-import BlogsMain from './Components/Blogs/BlogsMain';
+// import BlogsMain from './Components/Blogs/BlogsMain';
 
 
 
@@ -27,7 +27,7 @@ function App() {
         <Route path="about" element={<About />} />
 
         <Route path="community/:testId" element={<CommPages />} />
-        <Route path="blogsmain" element={<BlogsMain />} />
+        {/* <Route path="blogs" element={<Blogs />} /> */}
 
       </Routes>
       <Footer/>
